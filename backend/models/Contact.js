03f39396801e804eba22dbf7ceb678a40d41d1cc
shiek abdullah s <shiekabdullah181@gmail.com>
2026-05-27 18:38:41 +0530

    Frontend deployment setup

--- backend/models/Contact.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const contactSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  message: String,
-});
-
-export default mongoose.model("Contact", contactSchema);
